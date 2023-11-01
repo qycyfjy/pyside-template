e258dc76d135caae771f594e9aa28e4de8c9dc97
qycyfjy <kyaruny@gmail.com>
2023-11-02 00:49:22 +0800

    tray

--- app.py
+++ app.py
@@ -17,8 +17,15 @@ import os
 import sys
 from typing import Optional
 
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtCore import Signal, Slot
+from PySide6.QtGui import QIcon, QAction, QPalette
+from PySide6.QtWidgets import (
+    QWidget,
+    QApplication,
+    QSystemTrayIcon,
+    QMenu,
+    QColorDialog,
+)
 
 import resources_rc
 import mainwindow_ui
@@ -30,8 +37,24 @@ class MainWindow(QWidget):
         self.ui = mainwindow_ui.Ui_Form()
         self.ui.setupUi(self)
 
+        self.main_icon = QIcon(":/icons/box.svg")
+
         self.setWindowTitle("Python ♥ Qt")
-        self.setWindowIcon(QIcon(":/icons/box.svg"))
+        self.setWindowIcon(self.main_icon)
+
+        self.tray = QSystemTrayIcon()
+        self.tray.setIcon(self.main_icon)
+        self.tray.setVisible(True)
+
+        self.tray_menu = QMenu()
+        self.copy_color_action = QAction("Change Color")
+        self.copy_color_action.triggered.connect(self.select_label_color)
+        self.tray_menu.addAction(self.copy_color_action)
+        self.tray_menu.addSeparator()
+        self.exit_action = QAction("Exit")
+        self.exit_action.triggered.connect(QApplication.exit)
+        self.tray_menu.addAction(self.exit_action)
+        self.tray.setContextMenu(self.tray_menu)
 
         self.ui.pushButton.clicked.connect(self.increment)
         self.ui.pushButton_2.clicked.connect(self.decrement)
@@ -53,6 +76,18 @@ class MainWindow(QWidget):
     def reset(self):
         self.label_value = 0
         self.update_label()
+
+    def select_label_color(self):
+        original_palette = self.ui.label.palette()
+        dialog = QColorDialog()
+        dialog.currentColorChanged.connect(self.set_label_color)
+        if not dialog.exec():
+            self.ui.label.setPalette(original_palette)
+
+    def set_label_color(self, color):
+        palette = self.ui.label.palette()
+        palette.setColor(QPalette.ColorRole.WindowText, color)
+        self.ui.label.setPalette(palette)
 
     def update_label(self):
         self.ui.label.setText(str(self.label_value))
